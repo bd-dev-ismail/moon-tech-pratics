073177f2bffb864a640a46bf291753e789888c3a
bd-dev-ismail <ismailratul7@gmail.com>
2023-03-29 15:54:25 +0600

    keep learning

---
+++ src/redux/rootReducer/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import { filterReducer } from "../reducers/filterReducer";
+
+import productReducer from "../reducers/productReducer";
+
+export const rootReducer = combineReducers({
+  product: productReducer,
+  filter: filterReducer,
+});
